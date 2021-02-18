5e877dd1ea1e93e6eba938d1f9267c5c0470ae11
jose Morales <jose.morales@televisatim.com>
2021-02-17 18:39:29 -0600

    ADD: Basic add ons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,26 @@
 const { nuxifyStorybook } = require('../.nuxt-storybook/storybook/main.js')
 
 module.exports = nuxifyStorybook({
-  webpackFinal (config, options) {
-
+  webpackFinal(config, options) {
     // extend config here
-    
+
     return config
   },
   stories: [
     // Add your stories here
   ],
   addons: [
-    // Add your addons here
-  ]
+    // {
+    //   name: '@storybook/addon-essentials',
+    //   options: {
+    //     backgrounds: false,
+    //     configureJSX: true,
+    //     babelOptions: {},
+    //     sourceLoaderOptions: null,
+    //   },
+    // },
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+    '@whitespace/storybook-addon-html',
+  ],
 })
