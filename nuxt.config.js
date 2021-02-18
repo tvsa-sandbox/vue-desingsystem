9971e408a0cf332595b91a8341eef14d40cc0cfd
jose Morales <jose.morales@televisatim.com>
2021-02-17 21:33:32 -0600

    add: config render vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, ctx) {
+      config.resolve.alias.vue = 'vue/dist/vue.common'
+    },
+  },
 }
